4b255abb210b0b9bb00488f221e82e8d2cc038f7
Joshua <noll.joshua@outlook.com>
2025-12-30 16:31:17 -0500

    add lsps to mason tool installer

--- lua/plugins/lsp/format.lua
+++ lua/plugins/lsp/format.lua
@@ -1,4 +1,4 @@
-return { -- Autoformat
+return {
   'stevearc/conform.nvim',
   event = { 'BufWritePre' },
   cmd = { 'ConformInfo' },
@@ -29,10 +29,6 @@ return { -- Autoformat
       end
     end,
     formatters_by_ft = {
-      -- Conform can also run multiple formatters sequentially
-      -- python = { "isort", "black" },
-      --
-      -- You can use 'stop_after_first' to run the first available formatter from the list
       lua = { 'stylua' },
       javascript = { 'prettierd', 'prettier', stop_after_first = true },
       javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
