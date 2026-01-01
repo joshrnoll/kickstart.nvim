e60f28a589780d7e3486039fba74dbd984d7ed3a
Joshua <noll.joshua@outlook.com>
2026-01-01 09:26:38 -0500

    update sidekick config

--- lua/editor/completion.lua
+++ lua/editor/completion.lua
@@ -1,63 +1,63 @@
 return {
-  'saghen/blink.cmp',
-  event = 'VimEnter',
-  version = '1.*',
-  dependencies = {
-    -- Snippet Engine
-    {
-      'L3MON4D3/LuaSnip',
-      version = '2.*',
-      build = (function()
-        return 'make install_jsregexp'
-      end)(),
-      dependencies = {
-        -- Snippet library
-        {
-          'rafamadriz/friendly-snippets',
-          config = function()
-            require('luasnip.loaders.from_vscode').lazy_load()
-          end,
-        },
-      },
-      opts = {},
-    },
-    'folke/lazydev.nvim',
-  },
-  --- @module 'blink.cmp'
-  --- @type blink.cmp.Config
-  opts = {
-    keymap = {
-      -- I changed to the 'enter' preset, but 'default' is generally recommended.
-      -- See `:help ins-completion`
-      preset = 'enter',
-    },
+	"saghen/blink.cmp",
+	event = "VimEnter",
+	version = "1.*",
+	dependencies = {
+		-- Snippet Engine
+		{
+			"L3MON4D3/LuaSnip",
+			version = "2.*",
+			build = (function()
+				return "make install_jsregexp"
+			end)(),
+			dependencies = {
+				-- Snippet library
+				{
+					"rafamadriz/friendly-snippets",
+					config = function()
+						require("luasnip.loaders.from_vscode").lazy_load()
+					end,
+				},
+			},
+			opts = {},
+		},
+		"folke/lazydev.nvim",
+	},
+	--- @module 'blink.cmp'
+	--- @type blink.cmp.Config
+	opts = {
+		keymap = {
+			-- I changed to the 'enter' preset, but 'default' is generally recommended.
+			-- See `:help ins-completion`
+			preset = "enter",
+		},
 
-    appearance = {
-      nerd_font_variant = 'mono',
-    },
+		appearance = {
+			nerd_font_variant = "mono",
+		},
 
-    completion = {
-      -- By default, `<c-space>` shows documentation (when the completion window is open)
-      -- Optionally, set `auto_show = true` to show the documentation after a delay.
-      documentation = { auto_show = true, auto_show_delay_ms = 500 },
-    },
+		completion = {
+			-- By default, `<c-space>` shows documentation (when the completion window is open)
+			-- Optionally, set `auto_show = true` to show the documentation after a delay.
+			documentation = { auto_show = true, auto_show_delay_ms = 500 },
+		},
 
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'lazydev' },
-      providers = {
-        lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
-      },
-      per_filetype = {
-        codecompanion = { 'codecompanion' },
-      },
-    },
+		sources = {
+			default = { "lsp", "path", "snippets", "lazydev" },
+			providers = {
+				lazydev = { module = "lazydev.integrations.blink", score_offset = 100 },
+			},
+			per_filetype = {
+				codecompanion = { "codecompanion" },
+			},
+		},
 
-    snippets = { preset = 'luasnip' },
+		snippets = { preset = "luasnip" },
 
-    -- See :h blink-cmp-config-fuzzy for more information
-    fuzzy = { implementation = 'lua' },
+		-- See :h blink-cmp-config-fuzzy for more information
+		fuzzy = { implementation = "lua" },
 
-    -- Shows a signature help window while you type arguments for a function
-    signature = { enabled = true },
-  },
+		-- Shows a signature help window while you type arguments for a function
+		signature = { enabled = true },
+	},
 }
