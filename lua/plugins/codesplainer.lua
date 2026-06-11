a10fdb4103d66560469eb5c96028e99557017a3b
Josh Noll <joshua.r.noll@gmail.com>
2026-06-11 17:15:10 -0500

    feat: toggle codesplainer chat

--- lua/plugins/codesplainer.lua
+++ lua/plugins/codesplainer.lua
@@ -1,28 +1,28 @@
 return {
-  url = "https://github.com/joshrnoll/codesplainer",
-  name = "codesplainer.nvim",
-  lazy = true,
-  cmd = { "CodesplainerAsk", "CodesplainerShow", "CodesplainerHide", "CodesplainerClear", "Codesplainer" },
-  keys = {
-    {
-      "<leader>la",
-      ":CodesplainerAsk ",
-      mode = "v",
-      desc = "Ask Codesplainer about selection",
-    },
-    {
-      "<leader>ls",
-      "<Cmd>CodesplainerShow<CR>",
-      mode = "n",
-      desc = "Show Codesplainer",
-    },
-  },
-  opts = {
-    provider = "openai",
-    openai = {
-      api_key_env = "OPENROUTER_API_KEY",
-      endpoint = "https://openrouter.ai/api/v1/chat/completions",
-      model = "google/gemini-3.1-flash-lite",
-    },
-  },
+	url = "https://github.com/joshrnoll/codesplainer",
+	name = "codesplainer.nvim",
+	lazy = true,
+	cmd = { "CodesplainerAsk", "CodesplainerClear", "Codesplainer" },
+	keys = {
+		{
+			"<leader>ca",
+			"<Cmd>CodesplainerAsk<CR>",
+			mode = "v",
+			desc = "Ask Codesplainer about selection",
+		},
+		{
+			"<leader>cs",
+			"<Cmd>Codesplainer<CR>",
+			mode = "n",
+			desc = "Toggle Codesplainer",
+		},
+	},
+	opts = {
+		provider = "openai",
+		openai = {
+			api_key_env = "OPENROUTER_API_KEY",
+			endpoint = "https://openrouter.ai/api/v1/chat/completions",
+			model = "google/gemini-3.1-flash-lite",
+		},
+	},
 }
