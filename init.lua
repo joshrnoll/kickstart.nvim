101d7874d3294ed2d40a2d592b523d5e538a1746
Josh Noll <josh.noll@unstructured.io>
2026-07-21 12:24:28 -0500

    fix(lazy): set leaders before setup

--- init.lua
+++ init.lua
@@ -1,3 +1,10 @@
+-- Set leaders before loading lazy.nvim so plugin keymaps use the correct prefix.
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Set to true if you have a Nerd Font installed and selected in the terminal.
+vim.g.have_nerd_font = false
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
